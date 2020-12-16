c7ce4556528f0278eb712badbd3645a37a44c963
Raph <49836486+raphgilles@users.noreply.github.com>
2020-12-16 18:57:11 +0100

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -3,7 +3,7 @@ do
 for i in *.mp4; do
   ffmpeg -re -y -i "$i" -vcodec libx264 -b:v 600k -r 24 -s 640x360 \
 -filter:v yadif -ab 128k -ac 1 -ar 44100 -f flv \
-"rtmp://peertube.stream:1935/live/CLÉ-DU-STREAM"
+"rtmp://URL-DE-L-INSTANCE:1935/live/CLÉ-DU-STREAM"
 sleep 5
 done
 done
