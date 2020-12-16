28b7f3e1b318ed342701d736a6868b04f84556f1
Raph <49836486+raphgilles@users.noreply.github.com>
2020-12-16 20:41:06 +0100

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -9,7 +9,7 @@ CONCAT=$(echo $(ls *.ts) | sed -e "s/ /|/g")
 
   ffmpeg -re -y -i "concat:$CONCAT" -vcodec libx264 -b:v 600k -r 24 -s 640x360 \
 -filter:v yadif -ab 128k -ac 1 -ar 44100 -f flv \
-"rtmp://peertube.stream:1935/live/dc251de2-9075-416a-a81f-a402bb6ca8ad"
+"rtmp://LINK-OF-THE-STREAM/live/KEY-OF-THE-STREAM"
 sleep 5
 
 find . -name "*.ts" -type f -delete
