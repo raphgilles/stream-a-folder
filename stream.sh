4e551f26e91ec6c85fe144a7e68ce136334282c8
Raph <49836486+raphgilles@users.noreply.github.com>
2020-12-16 22:58:36 +0100

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,16 +1,16 @@
 while [ 1 ];
 do
 
+find . -name "*.ts" -type f -delete
+
 for f in $(ls *.mp4); do
     ffmpeg -i $f -c copy -bsf:v h264_mp4toannexb -f mpegts $f.ts
 done
 
 CONCAT=$(echo $(ls *.ts) | sed -e "s/ /|/g")
 
-  ffmpeg -re -y -i "concat:$CONCAT" -vcodec libx264 -b:v 600k -r 24 -s 640x360 \
+  ffmpeg -re -y -i "concat:$CONCAT" -vf select=concatdec_select,yadif -af aselect=concatdec_select,aresample=async=1 -vcodec libx264 -b:v 600k -r 24 -s 640x360 \
 -filter:v yadif -ab 128k -ac 1 -ar 44100 -f flv \
 "rtmp://LINK-OF-THE-STREAM/live/KEY-OF-THE-STREAM"
-
-find . -name "*.ts" -type f -delete
 
 done
