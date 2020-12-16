2c0eaa629f78f45c2786c1ce86dcb88175dc1dcd
Raph <49836486+raphgilles@users.noreply.github.com>
2020-12-17 00:25:44 +0100

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 while [ 1 ];
 do
 
@@ -9,8 +11,8 @@ done
 
 CONCAT=$(echo $(ls *.ts) | sed -e "s/ /|/g")
 
-  ffmpeg -re -y -i "concat:$CONCAT" -vf select=concatdec_select,yadif -af aselect=concatdec_select,aresample=async=1 -vcodec libx264 -b:v 600k -r 24 -s 640x360 \
--filter:v yadif -ab 128k -ac 1 -ar 44100 -f flv \
+  ffmpeg -re -y -i "concat:$CONCAT" -vf "select=concatdec_select,yadif" -af "aselect=concatdec_select,aresample=async=1" -vcodec libx264 -b:v 600k -r 24 -s 640x360 \
+ -ab 128k -ac 1 -ar 44100 -f flv \
 "rtmp://LINK-OF-THE-STREAM/live/KEY-OF-THE-STREAM"
 
 done
