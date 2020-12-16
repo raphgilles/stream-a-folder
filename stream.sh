dd9f69d65f01d6f0663ea6471f7256c8a6c517a1
Raph <49836486+raphgilles@users.noreply.github.com>
2020-12-16 21:03:12 +0100

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -10,7 +10,6 @@ CONCAT=$(echo $(ls *.ts) | sed -e "s/ /|/g")
   ffmpeg -re -y -i "concat:$CONCAT" -vcodec libx264 -b:v 600k -r 24 -s 640x360 \
 -filter:v yadif -ab 128k -ac 1 -ar 44100 -f flv \
 "rtmp://LINK-OF-THE-STREAM/live/KEY-OF-THE-STREAM"
-sleep 5
 
 find . -name "*.ts" -type f -delete
 
