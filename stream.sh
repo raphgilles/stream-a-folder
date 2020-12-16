a9284488465147e1522cad6122ab451938984304
Raph <49836486+raphgilles@users.noreply.github.com>
2020-12-16 18:53:47 +0100

    Add files via upload

---
+++ stream.sh
@@ -0,0 +1,9 @@
+while [ 1 ];
+do
+for i in *.mp4; do
+  ffmpeg -re -y -i "$i" -vcodec libx264 -b:v 600k -r 24 -s 640x360 \
+-filter:v yadif -ab 128k -ac 1 -ar 44100 -f flv \
+"rtmp://peertube.stream:1935/live/CLÉ-DU-STREAM"
+sleep 5
+done
+done
