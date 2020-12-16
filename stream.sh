07660b1ff70c1814093b6d6b7af0b7a453976507
Raph <49836486+raphgilles@users.noreply.github.com>
2020-12-16 20:39:42 +0100

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,9 +1,17 @@
 while [ 1 ];
 do
-for i in *.mp4; do
-  ffmpeg -re -y -i "$i" -vcodec libx264 -b:v 600k -r 24 -s 640x360 \
--filter:v yadif -ab 128k -ac 1 -ar 44100 -f flv \
-"rtmp://URL-DE-L-INSTANCE:1935/live/CLÉ-DU-STREAM"
-sleep 5
+
+for f in $(ls *.mp4); do
+    ffmpeg -i $f -c copy -bsf:v h264_mp4toannexb -f mpegts $f.ts
 done
+
+CONCAT=$(echo $(ls *.ts) | sed -e "s/ /|/g")
+
+  ffmpeg -re -y -i "concat:$CONCAT" -vcodec libx264 -b:v 600k -r 24 -s 640x360 \
+-filter:v yadif -ab 128k -ac 1 -ar 44100 -f flv \
+"rtmp://peertube.stream:1935/live/dc251de2-9075-416a-a81f-a402bb6ca8ad"
+sleep 5
+
+find . -name "*.ts" -type f -delete
+
 done
